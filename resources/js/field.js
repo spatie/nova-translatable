9e1321f522b2a7c1d43cb39bac1ecb5f11a49a4c
freek <freek@spatie.be>
2018-10-24 09:42:21 +0200

    remove resources

--- resources/js/field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting(Vue => {
-    Vue.component('index-nova-translatable', require('./components/Nova/IndexField'));
-    Vue.component('detail-nova-translatable', require('./components/Nova/DetailField'));
-    Vue.component('form-nova-translatable', require('./components/Nova/FormField'));
-});
